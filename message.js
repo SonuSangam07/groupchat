b021df27d3860dfcc8610fc6203177515013c037
SonuSangam7 <sonusangamcsk1@gmail.com>
2022-10-14 15:42:09 +0530

    chat ready

--- message.js
+++ message.js
@@ -1,47 +1,93 @@
-function sendmsg(event) {
-  event.preventDefault();
-  let message = document.getElementById("message").value;
-  let obj = {
-    message: message,
-  };
-  const token = localStorage.getItem("token");
-  axios
-    .post("http://localhost:3000/sendmsg", obj, {
-      headers: { Authorization: token },
-    })
-    .then((response) => {
-      alert("Message Sent");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const token = localStorage.getItem('token');
+
+window.addEventListener('DOMContentLoaded', (e) => {
+    var sendbtn = document.getElementById('sendbtn');
+    sendbtn.addEventListener('click', sendMsg);
+    e.preventDefault();
+    // setInterval(()=>{
+    //     showMsg();
+    // },1000);
+     showMsg();
+    
+    
+
+    
+})
+
+
+function sendMsg(){
+    var msg = document.getElementById('msg').value;
+
+        let msgDetails = {
+            token: token,
+            message: msg,
+        }
+
+        axios.post('http://localhost:3000/sendmsg', msgDetails , {
+            headers: {"Authorization":token}
+        })
+        .then(() => {
+            document.getElementById('msg').value = "";
+        })
+}
+
+function showMsg(){
+    const token = localStorage.getItem('token');
+    axios.get('http://localhost:3000/getmessages', {headers: {"Authorization":token}})
+        .then(response => {
+            //console.log(response.data.data)
+            if(response.status === 200){
+                document.getElementById('showMsg').innerHTML="";
+                response.data.data.forEach(message => {
+                    var parentElemnt = document.getElementById('showMsg');
+                    var msgId = `${message.id}`;
+                    //console.log(msgId);
+                    parentElemnt.innerHTML += `
+                    <div class="message" id=${msgId}>
+                        <p class="meta">${message.Username}</p>
+                        <p class="text">
+                            ${message.message}
+                        </p>
+                    </div>`
+                })
+            }else{
+                throw new Error();
+            }
+        })
 }
 
 // retrieving messages
 
 window.addEventListener("DOMContentLoaded", (event) => {
   event.preventDefault();
+  const token=localStorage.getItem('token')
+  //console.log(token)
   axios
-    .get("http://localhost:3000/getmessages")
+    .get("http://localhost:3000/getmessages",{headers: {"Authorization": token}})
     .then((response) => {
       if (response.status == 200) {
-        console.log(response)
-        let inbox = document.getElementById('inbox')
+       // console.log(response.data.data)
+      
         
-        let content1=`<div>you joined </div`
-          inbox.innerHTML+=content1;
-        for (let i = 0; i < response.data.data.length; i++) {
-          let message = response.data.data[i].message;
-          let id = response.data.data[i].name
-          
-         let content = `<div class="info"><span class="allmsgs">${id} :</span><span class="allmsgs">${message}</span></div>`
-         inbox.innerHTML += content
-        }
-      } else {
+        response.data.data.forEach(message => {
+          var parentElemnt = document.getElementById('showMsg');
+                    var msgId = `${message.id}`;
+                    //console.log(msgId);
+                    parentElemnt.innerHTML += `
+                    <div class="message" id=${msgId}>
+                        <p class="meta">${message.name}</p>
+                        <p class="text">
+                            ${message.message}
+                        </p>
+                    </div>`
+        })
+       
+         } else {
         throw new Error();
       }
     })
     .catch((err) => {
       console.log(err);
-    });
-});
+    })
+  })
+
